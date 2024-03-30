4fffeb4b99a8a89be4e8f195ad36f0e93aedec6d
huydu20 <vanhuydu123@gmail.com>
2024-03-30 15:36:07 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "parameters.h"
+
+using namespace std;
+
+class Person {
+
+};
+
+class Department {
+
+};
+
+class Employee {
+
+};
+
+class Company {
+	
+};
+
+int main () {
+	cout << "Hello World!";
+	return 0;
+}
